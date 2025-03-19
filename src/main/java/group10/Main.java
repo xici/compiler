3a08d9f44277ba452de6326877947a06f2781f8e
Bin <3067822655@qq.com>
2025-03-19 18:04:44 +0800

    添加Flat库以美化界面

--- src/main/java/group10/Main.java
+++ src/main/java/group10/Main.java
@@ -1,9 +1,12 @@
 package group10;
 
+import com.formdev.flatlaf.FlatLightLaf;
+
 import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) {
+        FlatLightLaf.setup();
         SwingUtilities.invokeLater(() -> {
             new LexerGUI().setVisible(true);
         });
